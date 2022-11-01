cfdba228be7eeae6713f2eeb47121f23e366ff4b
ninachen22 <116048270+ninachen22@users.noreply.github.com>
2022-11-01 17:56:25 -0400

    worked on table utilites

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
@@ -2,14 +2,21 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
+        return getMultiplicationTable(5);
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+        return getMultiplicationTable(10);
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= tableSize; i++) {
+            for (int j = 1; j <= tableSize; j++) {
+                sb.append(String.format("%3s |", i * j));
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
     }
 }
