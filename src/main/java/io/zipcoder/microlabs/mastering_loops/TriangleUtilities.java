692e5e7572b5b0e71d2e92c2bfe90974d3ec1dc6
ninachen22 <116048270+ninachen22@users.noreply.github.com>
2022-11-01 15:53:08 -0400

    reviewed and completed triangle utilities

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TriangleUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TriangleUtilities.java
@@ -3,25 +3,33 @@ package io.zipcoder.microlabs.mastering_loops;
 public class TriangleUtilities {
 
     public static String getTriangle(int numberOfRows) {
-        return null;
+        StringBuilder sb = new StringBuilder();
+
+        int idx = 1;
+        while (idx < numberOfRows) {
+            String thisRow = getRow(idx);
+            sb.append(thisRow).append("\n");
+            idx++;
+        }
+        return sb.toString();
     }
 
     public static String getRow(int numberOfStars) {
+        //StringBuilder sb = new StringBuilder();
+        //for (int i = 0; i < numberOfStars; i++) {
+        //   sb.append("*");
+        //}
+        //return sb.append();
         String buildingBlock = "*"; //created a string for the *
         String makeRow = buildingBlock.repeat(numberOfStars); //created string that repeats my buildingBlock string by the  numberOfStars
         return makeRow;
     }
 
     public static String getSmallTriangle() {
-        String buildingBlock = "*";
-        Integer smallTriSize = 4;
-        for (int i = 1; i <= smallTriSize; i++) {
-            String smallTri = buildingBlock.repeat(i+1);
-        } return null;
-//        return null;
+        return getTriangle(5);
     }
 
     public static String getLargeTriangle() {
-        return null;
+        return getTriangle(10);
     }
 }
